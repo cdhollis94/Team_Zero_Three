d39029bf99367c5937d5271fb19f956eefdc28e4
OneWayGondola <37632494+OneWayGondola@users.noreply.github.com>
2020-11-07 15:14:11 -0600

    Updated main with form dependencies, accounts bps

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -15,18 +15,34 @@ from main_app.models.ingredient import Ingredient
 from main_app.models.attribute import Attribute
 from main_app.models.associations import ing_att_assc
 from main_app.models.food_group import Food_Group
+from main_app.models.account import Account
 # --------------------------------------------------------------------------------------------------------------
 
 
 from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap(app)  # Initiate bootstrap to use on the templates
 
+# [WTForms]-----------------------------------------------------------------------------------------------------
+app.config['SECRET_KEY'] = 'Texas'
+# [flask-login]-------------------------------------------------------------------------------------------------
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login_bp.login'
 
 # [REGISTER BLUEPRINTS] ----------------------------------------------------------------------------------------
 #  - Templates used to handle individual routes used for different prats of the program
 from main_app.routes.test_blueprint import test_blueprint
 from main_app.routes.ingredient_bp import ingredient_bp
+from main_app.routes.signup_bp import signup_bp
+from main_app.routes.login_bp import login_bp
+from main_app.routes.login_success_bp import login_success_bp
+from main_app.routes.logout_bp import logout_bp
 from flask import render_template
 app.register_blueprint(test_blueprint)
 app.register_blueprint(ingredient_bp)
+app.register_blueprint(signup_bp)
+app.register_blueprint(login_bp)
+app.register_blueprint(login_success_bp)
+app.register_blueprint(logout_bp)
 # --------------------------------------------------------------------------------------------------------------
